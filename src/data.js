9bb38faa8083c6da4a8b799fc00693ed1762a709
Kenneth Onuorah <112291627+KennethOnuorah@users.noreply.github.com>
2022-10-05 14:29:27 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require("mongodb") // A mongo client to connect to the mongoDB database
 
 async function main(){
-    const uri = "mongodb+srv://S2068566010801s:Blackswordsman1@cluster0.hf5s9jl.mongodb.net/?retryWrites=true&w=majority"
+    const uri = ""
     const client = new MongoClient(uri) // Create instance of MongoClient
     try{
         await client.connect()
