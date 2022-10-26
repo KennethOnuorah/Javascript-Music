71fdcdb3a929389628cf2902ad9ec2494a45d905
kenneth4832@gmail.com <kenneth4832@gmail.com>
2022-10-26 15:51:31 -0500

    10-26-2022: Music controller, UI, fix for issue #8

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { validateAlbum, extractMetaAndStoreB64, createNewAlbum } from "../lib/saveManager.js"
+import { validateAlbum, extractMetaAndStoreB64, createNewAlbum, songOrderCorrection } from "../lib/saveManager.js"
 import { load } from "../lib/loadManager.js"
 import { openAlbumModal, modalOpen } from "../lib/albumModal.js"
 
 const loadingScreen = document.getElementById("loading_screen")
 const loadingScreenText = document.getElementById("loading_text")
 let isLoading = false
-
 const uploadBtn = document.getElementById("upload_album")
 
 window.addEventListener("load", () => {
@@ -25,6 +24,7 @@ uploadBtn.addEventListener("change", async(e) => {
         const albumName = e.target.files[0].webkitRelativePath.split('/')[0]
         console.log(await validateAlbum(albumName))
         console.log(await extractMetaAndStoreB64(e.target.files, loadingScreenText))
+        songOrderCorrection(albumName, loadingScreenText)
         console.log(await createNewAlbum(albumName, loadingScreenText))
         loadingScreen.style.opacity = "0"
         setTimeout(() => {
@@ -44,7 +44,7 @@ uploadBtn.addEventListener("change", async(e) => {
 /**
  * Update the list of albums the user has created
  */
-function updateAlbumList(){
+ function updateAlbumList(){
     const albumList = document.querySelectorAll(".grid_item")
     for (const album of albumList){
         album.addEventListener("click", function(){
